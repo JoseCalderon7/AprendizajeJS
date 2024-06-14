4bee882b574fa786ea828c04125fc7a2f5c665b8
JoSeCalderon1234 <tutuyosoy1111@gmail.com>
2024-06-14 15:41:28 -0400

    agregamos el control de flujo

---
+++ 03-Control-flujo/01-if.js
@@ -0,0 +1,9 @@
+// * if (condicion){
+//     * expresion
+// * }
+
+let edad = 19;
+
+if (edad > 18) {
+    console.log('Usuario mayor de edad');
+}
---
+++ 03-Control-flujo/02-else.js
@@ -0,0 +1,7 @@
+let edad = 12;
+
+if (edad > 18) {
+    console.log('Usuario mayor de edad');
+} else {
+    console.log('Usuario menor de edad');
+}
